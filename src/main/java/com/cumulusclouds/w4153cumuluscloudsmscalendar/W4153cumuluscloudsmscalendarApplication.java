fcd16712e1a5b5d1d4c665376d4ebd5033d1ab19
Ashley Garcia <ashley.garcia.official@gmail.com>
2024-12-08 18:03:55 -0500

    initial commit

---
+++ src/main/java/com/cumulusclouds/w4153cumuluscloudsmscalendar/W4153cumuluscloudsmscalendarApplication.java
@@ -0,0 +1,13 @@
+package com.cumulusclouds.w4153cumuluscloudsmscalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W4153cumuluscloudsmscalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W4153cumuluscloudsmscalendarApplication.class, args);
+	}
+
+}
